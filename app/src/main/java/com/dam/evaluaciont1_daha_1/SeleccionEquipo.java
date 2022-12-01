757550b80adefbe86cf686c7a36787db71bafe92
deveshaha <devesh_hanumante@outlook.com>
2022-12-01 13:57:49 +0100

    minor updates

--- app/src/main/java/com/dam/evaluaciont1_daha_1/SeleccionEquipo.java
+++ app/src/main/java/com/dam/evaluaciont1_daha_1/SeleccionEquipo.java
@@ -3,8 +3,19 @@ package com.dam.evaluaciont1_daha_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SeleccionEquipo extends AppCompatActivity {
+
+    Button btn_save, btn_clear;
+    Button btn_germany, btn_saudiArabia, btn_argentina, btn_australia, btn_belgium, btn_brazil,
+            btn_cameroon, btn_canada, btn_korea, btn_costaRica, btn_croatia, btn_denmark,
+            btn_ecuador, btn_spain, btn_usa, btn_france, btn_wales, btn_ghana,
+            btn_netherlands, btn_englad, btn_Iran, btn_japan, btn_morroco,
+            btn_mexico, btn_poland, btn_portugal, btn_qatar,
+            btn_Senegal, btn_serbia, btn_switzerland,
+            btn_tunisia, btn_uruguay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
